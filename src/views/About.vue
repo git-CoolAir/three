5a6d8d0d14787c31b5b3d44040a3cfee9281e920
git-CoolAir <970571959@qq.com>
2020-09-04 14:41:50 +0800

    master

---
+++ src/views/About.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="about">
+    <div class="three" ref="three"></div>
+  </div>
+</template>
+
+<script>
+import "@/scss/About/index.scss";
+// import * as THREE from "three";
+export default {
+  data() {
+    return {
+      Colors: {
+        red: 0xf25346,
+
+        white: 0xd8d0d1,
+
+        brown: 0x59332e,
+
+        pink: 0xf5986e,
+
+        brownDark: 0x23190f,
+
+        blue: 0x68c3c0,
+      },
+    };
+  },
+  methods: {
+    init() {
+      //创建绘画容器
+      // let canvas = this.$refs.three;
+    },
+  },
+  mounted() {
+    this.init();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.three {
+  position: absolute;
+
+  width: 100%;
+
+  height: 100%;
+
+  overflow: hidden;
+
+  background: linear-gradient(#e4e0ba, #f7d9aa);
+}
+</style>
